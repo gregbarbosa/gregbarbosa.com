eb330e68471780dd1a30643c78f051de711bce11
Greg Barbosa <gregbarbosa@me.com>
2026-01-07 23:01:27 -0500

    refactor: implement Starwind UI for navbar and footer

---
+++ src/components/starwind/aspect-ratio/index.ts
@@ -0,0 +1,10 @@
+import AspectRatio, {
+	aspectRatio,
+	aspectRatioWrapper,
+} from './AspectRatio.astro'
+
+const AspectRatioVariants = { aspectRatio, aspectRatioWrapper }
+
+export { AspectRatio, AspectRatioVariants }
+
+export default AspectRatio
